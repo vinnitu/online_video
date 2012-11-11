b86fc1e3fd66ff43e9093e39eb925874558f0671
Victor Y. Sklyar <vinnitu@gmail.com>
2012-11-11 17:14:14 +0200

    prettify File.js

--- javascript/File.js
+++ javascript/File.js
@@ -1,75 +1,65 @@
-var File ={}
+var File = {};
 
-File.test=function()
-    {
-        alert("File");
-    }
+File.test = function() {
+    alert("File");
+}
 
-File.loadArray=function(fileName,array)
-    {
-      var tUrl;
-      var fileSystemObj = new FileSystem();
-         var oFile=fileSystemObj.openCommonFile( fileName, "r");
-         if (oFile)
-            {
-               while(1)
-                {
-                    tUrl= oFile. readLine();
-                    if (tUrl==null) 
-                        break;
-                    array.push(tUrl);
-                 }
-                fileSystemObj.closeCommonFile(oFile);
+File.loadArray = function(fileName, array) {
+    var fileSystemObj = new FileSystem();
+
+    var oFile = fileSystemObj.openCommonFile(fileName, "r");
+    if (oFile) {
+        while(1) {
+            var tUrl = oFile.readLine();
+            if (tUrl == null) {
+                break;
             }
-    }
 
-File.saveArray=function(fileName,array)
-    {
-       var fileSystemObj = new FileSystem();
-       var oFile=fileSystemObj.openCommonFile( fileName, "w");
-         if (oFile)
-            {
-                for(var i=0; i < array.length; i++)
-                    oFile. writeLine(array[i]);
-                fileSystemObj.closeCommonFile(oFile);
-             }
+            array.push(tUrl);
+         }
+
+         fileSystemObj.closeCommonFile(oFile);
     }
-    
- File.addUrl=function(fileName,url,index)
-    {
-        var tempArray=new Array();
-         File.loadArray(fileName,tempArray)
-        
-        tempArray.splice(index,0,url);
-        File.saveArray(fileName,tempArray)
+}
+
+File.saveArray = function(fileName, array) {
+    var fileSystemObj = new FileSystem();
+
+    var oFile = fileSystemObj.openCommonFile(FileName, "w");
+    if (oFile) {
+        for (var i=0; i < array.length; i++) {
+            oFile. writeLine(array[i]);
+        }
+
+        fileSystemObj.closeCommonFile(oFile);
     }
- 
- File.delUrl=function(fileName,index)
-    {
-        var tempArray=new Array();
-         File.loadArray(fileName,tempArray)
-        
-        tempArray.splice(index,1);
-        File.saveArray(fileName,tempArray)
-    }
- 
-File.moveUrl=function(fileName, index, mov)
-    {
-        var tempArray=new Array();
-         File.loadArray(fileName,tempArray)
-        
-        var url = tempArray.splice(index,1);
-        tempArray.splice(index-mov,0, url.toString());
-        File.saveArray(fileName,tempArray)
-    }
- 
-File.changeUrl=function(fileName,url,index)
-    {
-        alert("change");
-        var tempArray=new Array();
-         File.loadArray(fileName,tempArray)
-        
-        tempArray.splice(index,1,url);
-        File.saveArray(fileName,tempArray)
-    }
-    
+}
+
+File.addUrl = function(fileName, url, index) {
+    var tempArray = new Array();
+    File.loadArray(fileName, tempArray)
+    tempArray.splice(index, 0, url);
+    File.saveArray(fileName, tempArray)
+}
+
+File.delUrl = function(fileName, index) {
+    var tempArray = new Array();
+    File.loadArray(fileName, tempArray)
+    tempArray.splice(index, 1);
+    File.saveArray(fileName, tempArray)
+}
+
+File.moveUrl = function(fileName, index, mov) {
+    var tempArray = new Array();
+    File.loadArray(fileName, tempArray)
+    var url = tempArray.splice(index, 1);
+    tempArray.splice(index-mov, 0, url.toString());
+    File.saveArray(fileName, tempArray)
+}
+
+File.changeUrl = function(fileName, url, index) {
+    var tempArray = new Array();
+    File.loadArray(fileName, tempArray)
+    tempArray.splice(index, 1, url);
+    File.saveArray(fileName, tempArray)
+}
