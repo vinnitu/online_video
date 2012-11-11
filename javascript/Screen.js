f37ac3b12f5d0aedff5a22e8caa23937fdac3f8b
Victor Y. Sklyar <vinnitu@gmail.com>
2012-11-11 17:01:54 +0200

    prettify Screen.js

--- javascript/Screen.js
+++ javascript/Screen.js
@@ -1,33 +1,21 @@
-var Screen ={plugin : null}
-alert("Screen");
+var Screen = {};
 
-Screen.init = function()
-    {
-       
-       var success = true;
-        this.plugin = document.getElementById("pluginScreen");
-        if (!this.plugin)
-            success = false;
-        return success;
+Screen.init = function() {
+    this.plugin = document.getElementById("pluginScreen");
+
+    return !!this.plugin;
+}
+
+Screen.changeMode = function() {
+   if (1 == pluginScreen.Flag3DEffectSupport()) {
+        var m = pluginScreen.Get3DEffectMode();
+
+        for (var i = 0; i < 7; ++i) {
+            var k = (m + i + 1) % 8;
+            if (1 == pluginScreen.Check3DEffectMode(k)) {
+                pluginScreen.Set3DEffectMode(k);
+                break;
+            }
+        }
     }
-
-Screen.changeMode = function()
-    {
-       if ( 1 == pluginScreen.Flag3DEffectSupport() )
-                {
-                    var m= pluginScreen.Get3DEffectMode();
-                    var k;
-                    for (var i = 0; i < 7; ++i) 
-                        {
-                            k=(m+i+1)%8;
-                            
-                            if( 1 == pluginScreen.Check3DEffectMode(k))
-                                {
-                                       pluginScreen.Set3DEffectMode(k);
-                                       break;
-                                }
-                        }
-                  
-                }
-    }
-
+}
